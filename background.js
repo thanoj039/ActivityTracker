c1f79c8e4f47f194b6936771007e0c5f665e71ef
Thanoj kumar <thanoj039@gmail.com>
2021-03-03 15:28:37 +0530

    functional modifications of popup pages

--- background.js
+++ background.js
@@ -1,46 +1,19 @@
 console.log('Hello from background.js');
 var st_time = (new Date).getTime()
+var cur_page = 1
 chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
 {
     if( request === "stopSession" )
     {
-        var numRequestsOutstanding = 0;
 
         chrome.history.search({
             'text': '',              
             'startTime': st_time  
         },
         function(historyItems) {
-            for (var i = 0; i < historyItems.length; ++i) {
-                var url = historyItems[i].url;
-                var processVisitsWithUrl = function(url) {
-                    return function(visitItems) {
-                        processVisits(url, visitItems);
-                    };
-                };
-                chrome.history.getVisits({url: url}, processVisitsWithUrl(url));
-                numRequestsOutstanding++;
-            }
+            console.log(historyItems)
         });
 
-
-        var urlToCount = {};
-
-        var processVisits = function(url, visitItems) {
-            for (var i = 0, ie = visitItems.length; i < ie; ++i) {
-                if (visitItems[i].transition != 'typed') {
-                    continue;
-                }
-
-                if (!urlToCount[url]) {
-                    urlToCount[url] = 0;
-                }
-
-                urlToCount[url]++;
-            }
-
-        }
-        console.log(urlToCount)
     }
 });
 
--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,39 @@
 var bg = chrome.extension.getBackgroundPage();
 
-var start_ele = document.getElementById("start")
-var stop_ele = document.getElementById("stop")
+window.onload=function(){
+    var view_ele = document.getElementById("view")
+    var start_ele = document.getElementById("start")
+    
+    view_ele.addEventListener("click",viewSession)
+    start_ele.addEventListener("click",startSession)
 
-start_ele.addEventListener("click",startSession)
-stop_ele.addEventListener("click",stopSession)
+}
+
+if(bg.cur_page==2){
+    const view_page = document.createElement("a")
+    view_page.href="session_view.html"
+    view_page.click()
+}else{
+    if(bg.cur_page==3){
+        const start_page = document.createElement("a")
+        start_page.href="session_run.html"
+        start_page.click()
+    }
+}
+
+function viewSession(){
+    bg.cur_page = 2
+    const view_page = document.createElement("a")
+    view_page.href="session_view.html"
+    view_page.click()
+    console.log("session details displayed.")
+}
 
 function startSession(){
     bg.st_time = (new Date).getTime()
+    bg.cur_page = 3
+    const start_page = document.createElement("a")
+    start_page.href="session_run.html"
+    start_page.click()
     console.log("session started")
-}
-
-function stopSession(){
-    chrome.runtime.sendMessage("stopSession")
-    console.log("session stopped")
 }
